3ff04f646ed5aa151a56134b5b8b91cab7e9219a
sidramwaseem <sidraw24@gmail.com>
2022-03-04 00:35:16 +0500

    final changes

---
+++ main.js
@@ -0,0 +1,10 @@
+const hamIcon = document.querySelector('.icon');
+ 
+
+hamIcon.addEventListener('click', (e)=>{
+  e.preventDefault();
+  document.querySelector('.body1').classList.add('display');
+  document.querySelector('.body2')
+
+
+})
